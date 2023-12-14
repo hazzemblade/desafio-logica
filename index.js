acfb4f85d8ec922cd842ee41e58c607cb23d4fac
Flavio De Jesus <127465612+hazzemblade@users.noreply.github.com>
2023-12-14 04:11:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ let nivelHeroi;
 
 // Lógica para determinar o nível do herói com base na experiência
 if (xpHeroi < 1000) {
-    nivelHeroi = "Ferro";
+    nivelHeroi = "RECRUTA 1";
 } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
-    nivelHeroi = "Bronze";
+    nivelHeroi = "SOLDADO";
 } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
-    nivelHeroi = "Prata";
+    nivelHeroi = "SOLDADO 2";
 } else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
-    nivelHeroi = "Ouro";
+    nivelHeroi = "CABO";
 } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
-    nivelHeroi = "Platina";
+    nivelHeroi = "SARGENTO";
 } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
-    nivelHeroi = "Ascendente";
+    nivelHeroi = "SARGENTO 2";
 } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
-    nivelHeroi = "Imortal";
+    nivelHeroi = "TENENTE";
 } else {
-    nivelHeroi = "Radiante";
+    nivelHeroi = "CAPITÃO";
 }
 
 // Exibição do resultado no console
