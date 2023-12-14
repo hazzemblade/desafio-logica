37c422c6f79d30be83faca94ab33fcbda5c4e46f
Flavio De Jesus <127465612+hazzemblade@users.noreply.github.com>
2023-12-14 04:04:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Variáveis para armazenar informações sobre o herói
+let nomeHeroi = "hazzemblade";
+let xpHeroi = 5000;
+let nivelHeroi;
+
+// Lógica para determinar o nível do herói com base na experiência
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+// Exibição do resultado no console
+console.log(`O herói chamado ${nomeHeroi} está no nível ${nivelHeroi}.`);
